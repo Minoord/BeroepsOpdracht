f35539efd7995ea6f8ebda8dd0d050fe9b98c232
Minoord <70703952+Minoord@users.noreply.github.com>
2020-10-30 08:24:59 +0100

    Mijn oorspronkelijke microbit eind opdracht

---
+++ mario met alleen letters.py
@@ -0,0 +1,63 @@
+from microbit import *
+import music
+
+for x in range(1):
+    music.play(["E","E","E"])
+for x in range(1):
+    music.play(["C","E","G","G3"])
+    
+for x in range(2):
+    music.play(["C","G","E"])
+    music.play(["A","B","B","A"])
+    music.play(["G","E","G","A"])
+    music.play(["F","G","E","C","D","B"])
+
+for x in range(1):
+    music.play(["G","F","F","D","E"])
+    music.play(["G","A","C"])
+    music.play(["G","F","F","F","D","E"])
+    music.play(["C","C","C"])
+for x in range(1):
+    music.play(["G","F","F","D","E"])
+    music.play(["G","A","C"])
+    music.play(["A","C","D"])
+    music.play(["D","D","C"])
+for x in range(1):
+    music.play(["C","C","C"])
+    music.play(["C","D","E","C","A","G"])
+    music.play(["C","C","C"])
+    music.play(["D","D","C"])
+for x in range(1):
+    music.play(["C","C","C"])
+    music.play(["C","D","E","C","A","G"])
+    music.play(["E","E","E"])
+    music.play(["C","E","G"])
+    music.play(["G"]) 
+
+for x in range(2):
+    music.play(["C","G","E"])
+    music.play(["A","B","Bb","A"])
+    music.play(["G","E","G","A"])
+    music.play(["F","G","E","C","D","B"])
+
+for x in range(1):
+    music.play(["E","C","G"])
+    music.play(["G","A","F","F","A"])
+    music.play(["B","A","A","A","G","F"])
+    music.play(["E","C","A","G"])
+for x in range(1):
+    music.play(["E","C","G"])
+    music.play(["G","A","F","F","A"])
+    music.play(["B","F","F","F","E","D","C"])
+    music.play(["G","E","C"])
+for x in range(1):
+    music.play(["C","G","E"])
+    music.play(["A","B","A"])
+    music.play(["G","B","G"])
+    music.play(["G","F","G"])
+    
+
+
+
+
+
